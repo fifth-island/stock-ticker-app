5b1989df3ae14d5b8021478cd0e416ad8554a2f1
fifth-island <113310477+fifth-island@users.noreply.github.com>
2022-12-02 01:16:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,45 +6,44 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/html'});
   if (req.url == "/") {
-	  res.write(`
-	  <h1>Hi! This is the home page</h1>
-	  <form action="/result" target="_blank" method="POST">
-	   <h2> Welcome to our Stock Ticker. </h2>
+	res.write(`
+		<h1>Hi! This is the home page</h1>
+		<form action="/result" target="_blank" method="POST">
+		<h2> Welcome to our Stock Ticker. </h2>
 
-	   <p> Select what type of input you want to use in the search </p>
+		<p> Select what type of input you want to use in the search </p>
 
-	   <label>Company</label>
-	   <input type='radio' name='type_input' id="company_name" value="company">
+		<label>Company</label>
+		<input type='radio' name='type_input' id="company_name" value="company">
 
-	   <label>Ticker</label>
-	   <input type='radio' name='type_input' id="company_ticker" value="ticker">
+		<label>Ticker</label>
+		<input type='radio' name='type_input' id="company_ticker" value="ticker">
 
-	   <p> Now, provide a word to check in our database </p>
+		<p> Now, provide a word to check in our database </p>
 
-	   <label> Search for keyword: </label>
-	   <input type='text' name='user_input'>
+		<label> Search for keyword: </label>
+		<input type='text' name='user_input'>
 
 
-	   <input type='submit' name='form_ticker' value='Submit'>
-	  `);
-	  res.end();
+		<input type='submit' name='form_ticker' value='Submit'>
+	`);
+	res.end();			
    
   } else if (req.url == '/result') {
-	  res.write ("Process the form<br>");
-			 res.write ("Process the form<br>");
-			 pdata = "";
-			 req.on('data', data => {
-		   pdata += data.toString();
-		 });
+	res.write ("Process the form<br>");
+	pdata = "";
+	req.on('data', data => {
+		pdata += data.toString();
+	});
 
-			// when complete POST data is received
-			req.on('end', () => {
-				pdata = qs.parse(pdata);
-				res.write ("The name is: "+ pdata['user_input']);
-				res.end();
-			});
+	// when complete POST data is received
+	req.on('end', () => {
+		pdata = qs.parse(pdata);
+		res.write ("The type chosen is: " + pdata['type_input']);
+		res.write ("The name is: " + pdata['user_input']);
+		res.end();
+	});
   }
-// 	res.end();
 }).listen(port);
 
 
