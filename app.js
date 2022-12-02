30510c2156cb9c639e4cb07db422d592bfe1af88
fifth-island <113310477+fifth-island@users.noreply.github.com>
2022-12-02 01:06:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,62 +5,44 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/html'});
-  if (req.url == "/")
-  res.write(`
-  <h1>Hi! This is the home page</h1>
-  <form action="/result" target="_blank" method="POST">
-   <h2> Welcome to our Stock Ticker. </h2>
+  if (req.url == "/") {
+	  res.write(`
+	  <h1>Hi! This is the home page</h1>
+	  <form action="/result" target="_blank" method="POST">
+	   <h2> Welcome to our Stock Ticker. </h2>
 
-   <p> Select what type of input you want to use in the search </p>
+	   <p> Select what type of input you want to use in the search </p>
 
-   <label>Company</label>
-   <input type='radio' name='type_input' id="company_name" value="company">
+	   <label>Company</label>
+	   <input type='radio' name='type_input' id="company_name" value="company">
+
+	   <label>Ticker</label>
+	   <input type='radio' name='type_input' id="company_ticker" value="ticker">
+
+	   <p> Now, provide a word to check in our database </p>
+
+	   <label> Search for keyword: </label>
+	   <input type='text' name='user_input'>
+
+
+	   <input type='submit' name='form_ticker' value='Submit'>
+	  `);
    
-   <label>Ticker</label>
-   <input type='radio' name='type_input' id="company_ticker" value="ticker">
+  } else if (req.url == '/result') {
+	  res.write ("Process the form<br>");
+			 res.write ("Process the form<br>");
+			 pdata = "";
+			 req.on('data', data => {
+		   pdata += data.toString();
+		 });
 
-   <p> Now, provide a word to check in our database </p>
-
-   <label> Search for keyword: </label>
-   <input type='text' name='user_input'>
-
-
-   <input type='submit' name='form_ticker' value='Submit'>
-  `);
-  
-//     res.setHeader('Content-Type', 'text/html');
-//   res.write("<h1>Fill this out</h1>");
-//   res.write("<form action='/username' method='post'><label> Enter user name: </label>");
-//   res.write("<input type='text' name'username'/>");
-//   res.write("<input type='submit' value='send' />");
-//   res.write("</form>");
-  
-  
-//   res.write(`
-//   <!doctype html><html><head><title> Stock Ticker </title></head><body><form action="/username" method="post"><div><labelEnter user name:</label>
-//   <input type="text" name="username"/>
-//   </div>
-//   <input type="submit" value="send" />
-//   </div>
-//   </form>
-//   </body>
-//   </html>
-//   `);
-//    return setHomePage(req, res);
-  else if (req.url == '/result')
-  res.write ("Process the form<br>");
-		 res.write ("Process the form<br>");
-		 pdata = "";
-		 req.on('data', data => {
-           pdata += data.toString();
-         });
-
-		// when complete POST data is received
-		req.on('end', () => {
-			pdata = qs.parse(pdata);
-			res.write ("The name is: "+ pdata['user_input']);
-			res.end();
-		});
+			// when complete POST data is received
+			req.on('end', () => {
+				pdata = qs.parse(pdata);
+				res.write ("The name is: "+ pdata['user_input']);
+				res.end();
+			});
+  } 
 }).listen(port);
 
 
