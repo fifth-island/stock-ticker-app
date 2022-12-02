761a13915843cb8eb8be1451b844a4defc7e89ac
fifth-island <113310477+fifth-island@users.noreply.github.com>
2022-12-01 23:01:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/html'});
   if (req.url == "/")
-  res.write("This is the home page");
+  res.write("<h1>This is the home page</h1>");
 //     res.setHeader('Content-Type', 'text/html');
 //   res.write("<h1>Fill this out</h1>");
 //   res.write("<form action='/username' method='post'><label> Enter user name: </label>");
